097b3875b3e00a1f233391f750d4764f603908a3
AdetokunAdenike <adetokunadenike@gmail.com>
2024-12-01 13:39:32 +0100

    Fix: Correct stdin handling and address lint issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,9 +8,9 @@ const r1 = readline.createInterface({
 r1.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
 
-  r1.on('close', () => {
-    console.log('This important software is now closing');
-  });
-
   r1.close();
+});
+
+r1.on('close', () => {
+  console.log('This important software is now closing');
 });
