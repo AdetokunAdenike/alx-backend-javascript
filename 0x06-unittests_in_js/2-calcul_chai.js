5147612706cea302d500d91b797cc9e3f771cdaa
AdetokunAdenike <adetokunadenike@gmail.com>
2025-01-13 17:11:56 +0100

    Update

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,5 +1,4 @@
-#!/usr/bin/env node
-const calculateNumber = (type, a, b) => {
+export default function calculateNumber(type, a, b) {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
   }
@@ -10,6 +9,4 @@ const calculateNumber = (type, a, b) => {
     return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
   return 0;
-};
-
-export default calculateNumber;
+}
