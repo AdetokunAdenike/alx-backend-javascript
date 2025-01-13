92948190fa672cc43f06f76b0ad3de0a8d0d75ae
AdetokunAdenike <adetokunadenike@gmail.com>
2025-01-13 18:17:40 +0100

    Update

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,25 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should stub Utils.calculateNumber and spy on console.log', () => {
+
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
