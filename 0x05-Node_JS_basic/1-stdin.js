280b7fd3403b727da6b6748aa6915e9c5d57604a
AdetokunAdenike <adetokunadenike@gmail.com>
2025-01-17 23:22:46 +0100

    Update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,10 +5,8 @@ process.stdin.on('data', (data) => {
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
 
-// Ensure the program ends in piped input scenarios
-if (!process.stdin.isTTY) {
-  process.stdin.resume(); // Start reading the piped input
-}
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
+});
