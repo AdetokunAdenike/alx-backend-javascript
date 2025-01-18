17e915d3646f746f045782ab5414cc499a1b396e
AdetokunAdenike <adetokunadenike@gmail.com>
2025-01-18 05:01:15 +0100

    Update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello ALX!');
 });
 
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT);
 
 module.exports = app;
